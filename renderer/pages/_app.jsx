55192394f824d87347696ac2ee0f8381189aef43
Manyachnyii <manyachnyii@gmail.com>
2022-02-26 06:52:16 +0300

    Next app

---
+++ renderer/pages/_app.jsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import "bootstrap/dist/css/bootstrap.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>YouTube channels parser</title>
+      </Head>
+      <main className="d-flex align-content-center flex-wrap">
+        <Component {...pageProps} />
+      </main>
+      <style jsx>{`
+        main {
+          min-height: 100vh;
+        }
+      `}</style>
+    </>
+  );
+}
+
+export default MyApp;
