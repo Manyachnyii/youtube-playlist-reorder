8496d4b658f7337e86c60659e6920a39fdecf5ca
Manyachnyii <manyachnyii@gmail.com>
2022-04-24 14:27:01 +0300

    Parse errors

--- renderer/lib/getPlaylist.js
+++ renderer/lib/getPlaylist.js
@@ -1,13 +1,17 @@
 import ytpl from "ytpl";
 
 export const getPlaylist = async (url) => {
-  const valid = ytpl.validateID(url);
+  try {
+    const valid = ytpl.validateID(url);
 
-  if (valid) {
-    const playlistId = await ytpl.getPlaylistID(url);
-    const videoList = await ytpl(playlistId, { pages: 1 });
-    return { success: true, data: videoList };
+    if (valid) {
+      const playlistId = await ytpl.getPlaylistID(url);
+      const videoList = await ytpl(playlistId, { pages: 1 });
+      return { success: true, data: videoList };
+    }
+
+    throw { message: "Not valid input" };
+  } catch (error) {
+    return { success: false, error: error.message };
   }
-
-  return { success: false };
 };
