3327d4f3acc3bec9c6a280a3c7b0d6d0bfd8dfb5
Manyachnyii <manyachnyii@gmail.com>
2022-04-24 08:18:45 +0300

    Add images domains

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  images: {
+    domains: ["i.ytimg.com", "yt3.ggpht.com"],
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.target = "electron-renderer";
