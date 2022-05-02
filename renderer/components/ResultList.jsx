e8b3a388911b968ee3fbad5ed289aee45014a4e1
Manyachnyii <manyachnyii@gmail.com>
2022-05-02 20:19:40 +0300

    Convert next/image to img
    not support next/image loader

--- renderer/components/ResultList.jsx
+++ renderer/components/ResultList.jsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { Row, Col } from "reactstrap";
 import { Card, CardBody, CardTitle, CardText } from "reactstrap";
 
@@ -8,13 +7,8 @@ export const ResultList = ({
   <div className="my-4">
     <header>
       <div className="d-flex align-items-center">
-        <Image
-          src={author.bestAvatar.url}
-          width={author.bestAvatar.width}
-          height={author.bestAvatar.height}
-          layout="responsive"
-        />
-        <span className="h1">{author.name}</span>
+        <img src={author.bestAvatar.url} alt="avatar" />
+        <span className="h1 ms-2">{author.name}</span>
       </div>
       <p>
         <span className="h2">{title} </span>
@@ -26,12 +20,7 @@ export const ResultList = ({
       {items.map(({ title, id, duration, bestThumbnail }) => (
         <Col key={id}>
           <Card className="h-100">
-            <Image
-              src={bestThumbnail.url}
-              width={bestThumbnail.width}
-              height={bestThumbnail.height}
-              layout="responsive"
-            />
+            <img className="w-100" src={bestThumbnail.url} alt="thumbnail" />
             <CardBody className="d-flex flex-column">
               <CardTitle tag="h5">{title}</CardTitle>
               <CardText className="mt-auto">
--- renderer/next.config.js
+++ renderer/next.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  images: {
-    domains: ["i.ytimg.com", "yt3.ggpht.com"],
-  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.target = "electron-renderer";
